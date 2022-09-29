993ad3d5c2d876736e538ce9e02334d6c17cf1cf
subrat-bit <subratswain@cime.ac.in>
2022-09-30 00:36:15 +0530

    Initial Commit

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+    {
+        int a,b,sum;
+        printf("\nEnter First Number:");
+        scanf("%d",&a);
+        printf("\nEnter Second Number:");
+        scanf("%d",&b);
+        printf("\nSum Of Two Numbers:%d",a+b);
+        sum=a+b;
+        printf("\nSum Of Two Numbers Using Sum variable Is:%d",sum);
+        return 0;
+    }
